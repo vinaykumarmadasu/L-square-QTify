88a0fdd216877ad1fd81b8b603b0d791e0ee93b4
vinay kumar <madasuvinaykumar123@gmail.com>
2024-11-03 18:14:37 +0530

    with the latest changes

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import headphonesImage from "../../assets/hero_headphones.png";
+import headphonesImage from "../../assets/vibrating-headphone 1.png";
 
 function Hero() {
   return (
     <div className={styles.hero}>
-      {/* <div>
-        <h1>100 Thousand Songs, ad-free</h1>
-        <h1>Over thousands podcast episodes</h1>
-      </div> */}
+      <div>
+        <h2>100 Thousand Songs, ad-free</h2>
+        <h2>Over thousands podcast episodes</h2>
+      </div>
       <div>
         <img
-          src={headphonesImage}//{require("../../assets/hero_headphones.png")}
+          src={headphonesImage}//{require("../../assets/vibrating_headphone 1.png")}
          // width={212}
           alt="Headphones"
         />
       </div>
-    </div>
+   </div>
   );
 }
 
